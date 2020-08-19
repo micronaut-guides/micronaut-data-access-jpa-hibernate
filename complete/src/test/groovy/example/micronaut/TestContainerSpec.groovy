50a6ff1e2ebad45856ebae25bdb979a43e2dc609
niravassar <assarn@objectcomputing.com>
2020-08-19 14:04:56 -0500

    sample test

---
+++ complete/src/test/groovy/example/micronaut/TestContainerSpec.groovy
@@ -0,0 +1,15 @@
+package example.micronaut
+
+import spock.lang.Specification
+
+class TestContainerSpec extends Specification {
+
+    void 'sample run'() {
+        when:
+        String hello
+
+        then:
+        1 == 1
+    }
+
+}
